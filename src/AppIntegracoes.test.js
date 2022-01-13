6ed105ed73c7e724416043ea3ffd81f8da5103e2
gustavo <gustavoborges98@hotmail.com>
2022-01-12 21:54:32 -0300

    teste em componentes e inicio de testes a chamadas de api

---
+++ src/AppIntegracoes.test.js
@@ -0,0 +1,29 @@
+import React from 'react;'
+import api from './api';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+jest.mock('./api');
+
+describe('Requisições para API', () => {
+  it('Exibir lista de transações através da API', () => {
+    api.listaTransacoes.mockResolvedValue([
+      {
+        "valor": 10,
+        "transacao": "saque",
+        "data": "10/08/2020",
+        "id": 1
+      },
+      {
+        "transacao": "deposito",
+        "valor": "20",
+        "data": "26/09/2020",
+        "id": 2
+      },
+    ]);
+
+    render(<App />);
+
+    expect(screen.getByTestId('transacoes').children.length.toBe(2));
+  });
+});
